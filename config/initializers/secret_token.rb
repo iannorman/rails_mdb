2b8e1a2a56885df17e24bf23e1e837e73e5f528f
ian.norman <ian.norman@xbridge.com>
2011-05-10 13:57:41 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsMdb::Application.config.secret_token = 'c89bbd0f52ab3eb989b1e9071b08828de7a0073a6e0a5aa7feb3e0c7395e0342585f0c6df7d1910fce3ade7c86a815185374f93d6c10173f5940ef3660ffcad5'
